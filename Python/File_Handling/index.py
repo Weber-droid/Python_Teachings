d1254b8595c5e1a84c779da53e485dfaafe93963
Weber <emmanueltotimeh2002@gmail.com>
2025-03-27 09:20:30 +0000

    added

---
+++ Python/File_Handling/index.py
@@ -0,0 +1,11 @@
+f = open('web.txt', 'r')
+# print(f.read())
+
+# for x in f:
+#     print(x)
+
+# print(f.readline()) #reads first line
+# print(f.readline()) #reads second line
+# print(f.readline()) #reads third line
+
+# f.close() #close your file when you're done
